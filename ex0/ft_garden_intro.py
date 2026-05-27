95e21877dca9865b98633cb8196ce7dfa23d1df8
Vicente Lourenco Prista Von Bonhorst <vlourenc@c1r1s1.42lisboa.com>
2026-05-27 11:57:28 +0100

    doe

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,11 @@
+def main() -> None:
+    print("=== Welcome to My Garden ===")
+    name: str = "Rose"
+    height: str = "25cm"
+    age: str = "30 days"
+    print(f"Plant: {name}\nHeight: {height}\nAge: {age}")
+    print("=== End of Program ===")
+
+
+if __name__ == "__main__":
+    main()
